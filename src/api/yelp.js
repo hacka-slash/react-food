cef8d219a58cec95e41ca3dd3883b40f214e9637
placidohoff <placido.hoff@gmail.com>
2020-01-24 18:48:12 -0500

    Using the yelp object to make the request and output the number of results found

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,5 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 'Bearer zvdujbN1DmEh3qtRwpGZDPvFIhzthbrpxj4Uyi9vUoYuQHj2VVz15S-1pj8CtuQ0syRVxA_PBDYRWC8-7_iQjNLfjfJhL_7HZ1c9bQkUQmY3Vxg7zL7ao8gKqNojXnYx'
-    
     }
 });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,24 +1,38 @@
-//OVERVIEW: Connecting to the Yelp API
-//We are axios library over fetch even tho fetch is built in.
-    //npm i axios --save worked without error.
+//OVERVIEW: We make a call to the yelp API to return data based on our params on the button press
 
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     //the state uses '' empty string in the beggining. 
     const [term, setTerm] = useState('');
+    //Initialized as an empty array but will be filled with data from the response body
+    const [results, setResults] = useState([]);
 
+    
+    //asyncrhonous operation
+    const searchApi = async () => {
+         const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term:{term},
+                location: 'san jose'
+        }});
+        setResults(response.data.businesses);
+        console.log(response.data.businesses);
+    }
     return ( 
         <View>
             <SearchBar 
                 term={term}
-                onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={searchApi}
+                                
             />
             <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            <Text>We have found {results.length} results</Text>
         </View>
      );
 }
