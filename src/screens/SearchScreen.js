be602866bc59795102a4d845ed935e94cc1350c9
placidohoff <placido.hoff@gmail.com>
2020-01-23 22:43:12 -0500

    Making use of the useState() hook to modify children components when state change

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+    //the state uses '' empty string in the beggining. 
+    const [term, setTerm] = useState('');
+
     return ( 
         <View>
-            <SearchBar />
+            <SearchBar 
+                term={term}
+                onTermChange={newTerm => setTerm(newTerm)}
+            />
             <Text>Search Screen</Text>
+            <Text>{term}</Text>
         </View>
      );
 }
