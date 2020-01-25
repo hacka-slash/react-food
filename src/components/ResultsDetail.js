6d7c48526ac597529bb1f1a7f314f53fc4cc21bf
placidohoff <placido.hoff@gmail.com>
2020-01-25 14:12:29 -0500

    skipped

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 
-const ResultDetail = () => {
+const ResultDetail = ({result}) => {
     return (
         <View>
-            <Text>Results Detail</Text>
+            <Image 
+                source={{uri: result.image_url}}
+                style={styles.image}
+            />
+            <Text
+                style={styles.name}>{result.name}
+            </Text>
+            <Text>
+                {result.rating} Stars, {result.review_count} Reviews
+            </Text>
         </View>
       );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 14
+    }
+});
  
 export default ResultDetail;
