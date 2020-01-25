c3dae7ad7f5398727ee45e1889f3286b55ad0a76
placidohoff <placido.hoff@gmail.com>
2020-01-25 14:39:28 -0500

    Displaying images and making the style nice and prettier

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,7 +3,7 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 
 const ResultDetail = ({result}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Image 
                 source={{uri: result.image_url}}
                 style={styles.image}
@@ -19,10 +19,14 @@ const ResultDetail = ({result}) => {
 }
 
 const styles = StyleSheet.create({
+    container:{
+        marginLeft: 15
+    },
     image: {
         width: 250,
         height: 120,
-        borderRadius: 4
+        borderRadius: 4,
+        marginBottom: 5
     },
     name: {
         fontWeight: 'bold',
